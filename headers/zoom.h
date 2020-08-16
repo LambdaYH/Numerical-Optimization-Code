80f2c1ec8b8436aa9171a7052a22a3c381a0a8dc
Lambda_YH <chenyihang1998@gmail.com>
2020-08-16 20:36:21 +0800

    part of alogrithm,still lack of the realization of object funciton and interpolate function

--- headers/zoom.h
+++ headers/zoom.h
@@ -2,6 +2,10 @@
 #define ZOOM_H
 #include<math.h>
 double c1,c2;
+extern double object_function();
+extern double phi(double);
+extern double derivation_phi(double);
+extern double generate_aip1(const double, const double);
 double zoom(double alo,double ahi) {
 	while (true) {
 		auto aj = generate_aip1(alo, ahi);
@@ -20,18 +24,18 @@ double zoom(double alo,double ahi) {
 }
 //the object funtion f(x)
 double object_function() {
-
+	return 0;
 }
 //phi=f(xk+apk)
 double phi(double a) {
-
+	return 0;
 }
 //the derivation of phi
 double derivation_phi(double a) {
-
+	return 0;
 }
 //use interpolate to find a step length between a and b
 double generate_aip1(const double a,const double b) {
-
+	return 0;
 }
 #endif
