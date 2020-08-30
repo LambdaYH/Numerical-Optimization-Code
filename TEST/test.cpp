095065fb9eca502e9943c9aa17e3b0f8146ae52e
Lambda_YH <chenyihang1998@gmail.com>
2020-08-30 21:33:44 +0800

    try eigen

--- TEST/test.cpp
+++ TEST/test.cpp
@@ -2,18 +2,18 @@
 #include<iostream>
 using Eigen::VectorXd;
 using Eigen::MatrixXf;
-
+using std::cout;
+using std::endl;
 int main() {
-	Eigen::Vector4d xk;
-	MatrixXf pk(4,4);
-	for (int i = 0; i < 5; ++i) {
-		xk(i) = i;
-	}
-	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < 5; ++j) {
-			pk(i, j) = i + j;
-		}
-	}
-	auto temp = pk * pk;
-	std::cout << temp << std::endl;
+	Eigen::VectorXf a(3);
+	Eigen::MatrixXf b(3,3); //should init in this way
+	a << 1,
+		4,
+		7;
+	b << 1, 2, 3,
+		4, 5, 6,
+		7, 8, 9;
+	cout << b << endl;
+	cout << a.transpose() * b * a << endl;
+	
 }
