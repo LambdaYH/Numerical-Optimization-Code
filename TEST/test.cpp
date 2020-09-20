c5b7fb4941384283f107db09222f76de4555d1c2
Lambda_YH <chenyihang1998@gmail.com>
2020-09-20 20:07:41 +0800

    b

--- TEST/test.cpp
+++ TEST/test.cpp
@@ -1,9 +1,12 @@
 #include<Eigen/Dense>
 #include<iostream>
+#include<vector>
 using Eigen::VectorXd;
 using Eigen::MatrixXf;
 using std::cout;
 using std::endl;
+using namespace std;
+/*
 int main() {
 	Eigen::VectorXf a(3);
 	Eigen::MatrixXf b(3,3); //should init in this way
@@ -16,4 +19,15 @@ int main() {
 	cout << b << endl;
 	cout << a.transpose() * b * a << endl;
 	
+}
+*/
+int main() {
+	int i;
+	vector<int> v_i;
+	while (cin >> i) {
+		v_i.push_back(i);
+	}
+	for (auto& j : v_i) {
+		cout << j << endl;
+	}
 }
